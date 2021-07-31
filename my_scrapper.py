4b537992931019905cb92335b40579a278dcecdb
BauyrzhanS <87850126+BauyrzhanS@users.noreply.github.com>
2021-07-31 21:14:02 +0600

    Update my_scrapper.py

--- my_scrapper.py
+++ my_scrapper.py
@@ -2,9 +2,14 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 
 path = "https://github.com/trending"
+my_scraper(path)
 
-def my_req_get():
-    resp = req.get(path)
 
-def my_scraper():
+def my_req_get(path):
+    return req.get(path)
+
+def my_scraper(path):    
+    soup = bs(my_req_get(path).text, 'lxml')
+    print(soup.prettify())
+
     
